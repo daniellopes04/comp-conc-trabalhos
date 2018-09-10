6fb76a210e0dab5a3c5d96f0730b7f2c512d7299
Flávio Sousa <fn_maia@outlook.com>
2018-09-10 14:36:06 -0300

    Inicializadas variáveis globais

--- barbeiros.c
+++ barbeiros.c
@@ -2,12 +2,18 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-#define N //Numero de cadeiras
+#define N 5 //Numero de cadeiras
 
-pthread_mutex_t mutex;
-pthread_t 
+pthread_mutex_t room;
+pthread_t barber;
+pthread_t customer;
+sem_t seats;
 
 int main() {
-
+	int error = pthread_mutex_init(&room, NULL);
+	if(error) exit(1);
+	error = sem_init(&seats, 0, N);
+	if(error) exit(1);
 }
 
+void barber_action() {}
